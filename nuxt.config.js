f9c3267a0d32aedeebf80258c1ae1897d7d8b98e
Dennis Frijlink <dennisfrijlink@gmail.com>
2020-11-03 16:15:00 +0100

    deleted standard template data

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,16 +8,18 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'nuxt-spa-boilerplate',
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'nuxt-spa-boilerplate' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
   ],
